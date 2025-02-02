2e690e8752f5380102ba3fa84bc0506c4076fce5
suryaherdiyanto <suryaherdiyanto@gmail.com>
2025-02-02 17:00:39 +0800

    Feat: move parsing related to parser.go

--- extractor.go
+++ extractor.go
@@ -1,60 +1,8 @@
 package main
 
 import (
-	"strings"
-
 	"golang.org/x/net/html"
 )
-
-func TransverseDecendants(doc *html.Node, fn func(doc *html.Node)) {
-	for node := doc.FirstChild; node != nil; node = node.NextSibling {
-		fn(node)
-		if node.Type != html.ErrorNode {
-			TransverseDecendants(node, fn)
-		}
-	}
-}
-
-func GetTags(doc *html.Node, tagName string) []html.Node {
-	var nodes []html.Node
-
-	TransverseDecendants(doc, func(node *html.Node) {
-		if node.Type == html.ElementNode && node.DataAtom.String() == tagName {
-			nodes = append(nodes, *node)
-		}
-	})
-
-	return nodes
-}
-
-func GetAttribute(node *html.Node, name string) (html.Attribute, bool) {
-	for _, attr := range node.Attr {
-		if attr.Key == name {
-			return attr, true
-		}
-	}
-
-	return html.Attribute{}, false
-}
-
-func GetText(node *html.Node) (string, bool) {
-	text := ""
-
-	TransverseDecendants(node, func(n *html.Node) {
-		if n.Type == html.TextNode {
-			nodeText := strings.TrimSpace(n.Data)
-			if nodeText != "" {
-				text = nodeText
-			}
-		}
-	})
-
-	if text == "" {
-		return "", false
-	}
-
-	return text, true
-}
 
 func GetNewsLinks(doc *html.Node) []NewsLink {
 
--- parser.go
+++ parser.go
@@ -1,6 +1,11 @@
 package main
 
-import "time"
+import (
+	"strings"
+	"time"
+
+	"golang.org/x/net/html"
+)
 
 type NewsLink struct {
 	Title, Url string
@@ -12,4 +17,54 @@ type NewsArticle struct {
 	PublishedAt             time.Time
 	CreatedAt               time.Time
 	UpdatedAt               time.Time
+}
+
+func TransverseDecendants(doc *html.Node, fn func(doc *html.Node)) {
+	for node := doc.FirstChild; node != nil; node = node.NextSibling {
+		fn(node)
+		if node.Type != html.ErrorNode {
+			TransverseDecendants(node, fn)
+		}
+	}
+}
+
+func GetTags(doc *html.Node, tagName string) []html.Node {
+	var nodes []html.Node
+
+	TransverseDecendants(doc, func(node *html.Node) {
+		if node.Type == html.ElementNode && node.DataAtom.String() == tagName {
+			nodes = append(nodes, *node)
+		}
+	})
+
+	return nodes
+}
+
+func GetAttribute(node *html.Node, name string) (html.Attribute, bool) {
+	for _, attr := range node.Attr {
+		if attr.Key == name {
+			return attr, true
+		}
+	}
+
+	return html.Attribute{}, false
+}
+
+func GetText(node *html.Node) (string, bool) {
+	text := ""
+
+	TransverseDecendants(node, func(n *html.Node) {
+		if n.Type == html.TextNode {
+			nodeText := strings.TrimSpace(n.Data)
+			if nodeText != "" {
+				text = nodeText
+			}
+		}
+	})
+
+	if text == "" {
+		return "", false
+	}
+
+	return text, true
 }

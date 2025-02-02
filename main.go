b4fdece0d514f140ed4cfbaca42418435cd2667f
suryaherdiyanto <suryaherdiyanto@gmail.com>
2025-02-02 16:13:10 +0800

    Feat: fetch news source and extract links

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+
+	"golang.org/x/net/html"
+)
 
 func main() {
-	fmt.Println("test")
+	url, err := url.Parse(os.Args[1])
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Fetching: " + url.String())
+	request, err := http.Get(url.String())
+	fmt.Println("Fetched, with status code: " + request.Status)
+
+	if request.StatusCode != 200 {
+		panic(errors.New("Unexpected status code: " + request.Status))
+	}
+
+	doc, err := html.Parse(request.Body)
+	links := GetNewsLinks(doc)
+
+	fmt.Println(links)
+	fmt.Println(len(links))
 }

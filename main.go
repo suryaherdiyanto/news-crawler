c172257590728ad8e5f07fd154983804ca15bbe3
suryaherdiyanto <suryaherdiyanto@gmail.com>
2025-02-02 23:58:57 +0800

    Feat: using wait group to fetch index pages

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
 
 	"golang.org/x/net/html"
 )
@@ -15,20 +18,35 @@ func main() {
 	extractor := NewLinkExtractor(url)
 
 	fmt.Println("Fetching: " + url)
-	request, err := http.Get(url)
+	var links []NewsLink
+	wg := sync.WaitGroup{}
 
-	if err != nil {
-		panic(err)
+	for i := 1; i <= 100; i++ {
+		wg.Add(1)
+		time.Sleep(time.Millisecond + time.Duration(i*50))
+		if i == 1 {
+			url = url + "?page=1"
+		}
+
+		if i > 1 {
+			url = strings.Replace(url, "?page="+strconv.Itoa(i-1), "?page="+strconv.Itoa(i), 1)
+		}
+
+		go func(url string) {
+			request, err := http.Get(url)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Crawled <%s> Status code: %d\n", url, request.StatusCode)
+
+			doc, err := html.Parse(request.Body)
+			links = append(links, GetNewsLinks(doc, extractor)...)
+			wg.Done()
+		}(url)
 	}
-	fmt.Println("Fetched, with status code: " + request.Status)
 
-	if request.StatusCode != 200 {
-		panic(errors.New("Unexpected status code: " + request.Status))
-	}
+	wg.Wait()
 
-	doc, err := html.Parse(request.Body)
-	links := GetNewsLinks(doc, extractor)
-
-	fmt.Println(links)
-	fmt.Println(len(links))
+	fmt.Println("done")
+	fmt.Printf("Total links found: %d\n", len(links))
 }

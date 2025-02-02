a0d2e679da21e4c939afcd21c7d19557a213d3ec
suryaherdiyanto <suryaherdiyanto@gmail.com>
2025-02-02 14:59:19 +0800

    Feat: add test for GetAttribute

--- extractor_test.go
+++ extractor_test.go
@@ -23,3 +23,25 @@ func TestGetTags(t *testing.T) {
 		t.Errorf("Expected tags to be more than 0")
 	}
 }
+
+func TestGetAttribute(t *testing.T) {
+	f, _ := os.Open("./stub/cnn.html")
+	defer f.Close()
+
+	parsed, err := html.Parse(f)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	tag := GetTags(parsed, "a")[0]
+	attr, ok := GetAttribute(&tag, "href")
+
+	if !ok {
+		t.Errorf("Expected true, but got %v", ok)
+	}
+
+	if attr.Val == "" {
+		t.Errorf("Expected to be not empty")
+	}
+}

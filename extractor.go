a6fc41b199cd86dfd306844908c8fdc7cf763628
suryaherdiyanto <suryaherdiyanto@gmail.com>
2025-02-02 14:53:56 +0800

    Feat: added GetAttribute and GetNewsLinks utility

--- extractor.go
+++ extractor.go
@@ -25,9 +25,33 @@ func GetTags(doc *html.Node, tagName string) []html.Node {
 	return nodes
 }
 
+func GetAttribute(node *html.Node, name string) (html.Attribute, bool) {
+	for _, attr := range node.Attr {
+		if attr.Key == name {
+			return attr, true
+		}
+	}
+
+	return html.Attribute{}, false
+}
+
 func GetNewsLinks(doc *html.Node) []NewsLink {
 
 	var links []NewsLink
+
+	articles := GetTags(doc, "article")
+	for _, articleNode := range articles {
+		anchorNodes := GetTags(&articleNode, "a")
+
+		for _, anchorNode := range anchorNodes {
+			attr, ok := GetAttribute(&anchorNode, "href")
+			if !ok || attr.Val == "#" {
+				continue
+			}
+
+			links = append(links, NewsLink{Title: anchorNode.DataAtom.String(), Url: attr.Val})
+		}
+	}
 
 	return links
 }

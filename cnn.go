5724dbc755c85f7bd19bee5f6fedb4b97ef6c77c
suryaherdiyanto <suryaherdiyanto@gmail.com>
2025-02-08 20:47:22 +0800

    Feat: add cnn.go extractor

---
+++ cnn.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"slices"
+	"strings"
+	"time"
+
+	"golang.org/x/net/html"
+)
+
+type CnnNewsContent struct {
+	html *html.Node
+}
+
+func (c *CnnNewsContent) GetTitle() string {
+	tag := GetTags(c.html, "title")[0]
+	title, _ := GetText(&tag)
+
+	return title
+}
+
+func (c *CnnNewsContent) GetExcerpt() string {
+	return GetMeta(c.html, "description")
+}
+
+func (c *CnnNewsContent) GetContent() string {
+	divs := GetTags(c.html, "div")
+	var paragraphs []html.Node
+
+	for _, div := range divs {
+		attr, _ := GetAttribute(&div, "class")
+		classList := strings.Split(attr.Val, " ")
+
+		if slices.Index(classList, "data-text") > -1 {
+			paragraphs = GetTags(&div, "p")
+			break
+		}
+	}
+
+	var content string
+
+	for _, p := range paragraphs {
+		content += "<p>" + strings.TrimSpace(p.Data) + "</p>"
+	}
+
+	return content
+}
+
+func (c *CnnNewsContent) GetThumbnail() string {
+	tags := GetTags(c.html, "div")
+	var imageWrapper *html.Node
+	
+	for _, tag := range tags {
+		attr, _ := GetAttribute(&tag, "class")
+		classList := strings.Split(attr.Val, " ")
+
+		if slices.Index(classList, "detail-image") > -1 {
+			imageWrapper = &tag
+		}
+	}
+
+	if imageWrapper.DataAtom.String() == "" {
+		return ""
+	}
+
+	img := GetTags(imageWrapper, "img")[0]
+	attr, _ := GetAttribute(&img, "src")
+
+	return attr.Val
+}
+
+func (c *CnnNewsContent) GetPublishedAt() time.Time {
+	publishDate := GetMeta(c.html, "dtk:publishdate")
+	t, err := time.Parse("2006/01/02 15:04:05 MST", publishDate + " WIB")
+
+	if err != nil {
+		panic(err)
+	}
+
+	return t
+}
+
+func (c *CnnNewsContent) GetCreatedAt() time.Time {
+	publishDate := GetMeta(c.html, "dtk:createddate")
+	t, err := time.Parse("2006/01/02 15:04:05 MST", publishDate + " WIB")
+
+	if err != nil {
+		panic(err)
+	}
+
+	return t
+}
--- extractor.go
+++ extractor.go
@@ -3,8 +3,6 @@ package main
 import (
 	"net/http"
 	"net/url"
-	"slices"
-	"strings"
 	"time"
 
 	"golang.org/x/net/html"
@@ -28,10 +26,6 @@ type CnnNews struct {
 	Url string
 }
 
-type CnnNewsContent struct {
-	html *html.Node
-}
-
 func (c *CnnNews) GetArticleTags(node *html.Node) []html.Node {
 	return GetTags(node, "article")
 }
@@ -41,84 +35,6 @@ func (c *CnnNews) GetLinkText(node *html.Node) string {
 	return text
 }
 
-func (c *CnnNewsContent) GetTitle() string {
-	tag := GetTags(c.html, "title")[0]
-	title, _ := GetText(&tag)
-
-	return title
-}
-
-func (c *CnnNewsContent) GetExcerpt() string {
-	return GetMeta(c.html, "description")
-}
-
-func (c *CnnNewsContent) GetContent() string {
-	divs := GetTags(c.html, "div")
-	var paragraphs []html.Node
-
-	for _, div := range divs {
-		attr, _ := GetAttribute(&div, "class")
-		classList := strings.Split(attr.Val, " ")
-
-		if slices.Index(classList, "data-text") > -1 {
-			paragraphs = GetTags(&div, "p")
-			break
-		}
-	}
-
-	var content string
-
-	for _, p := range paragraphs {
-		content += "<p>" + strings.TrimSpace(p.Data) + "</p>"
-	}
-
-	return content
-}
-
-func (c *CnnNewsContent) GetThumbnail() string {
-	tags := GetTags(c.html, "div")
-	var imageWrapper *html.Node
-	
-	for _, tag := range tags {
-		attr, _ := GetAttribute(&tag, "class")
-		classList := strings.Split(attr.Val, " ")
-
-		if slices.Index(classList, "detail-image") > -1 {
-			imageWrapper = &tag
-		}
-	}
-
-	if imageWrapper.DataAtom.String() == "" {
-		return ""
-	}
-
-	img := GetTags(imageWrapper, "img")[0]
-	attr, _ := GetAttribute(&img, "src")
-
-	return attr.Val
-}
-
-func (c *CnnNewsContent) GetPublishedAt() time.Time {
-	publishDate := GetMeta(c.html, "dtk:publishdate")
-	t, err := time.Parse("2006/01/02 15:04:05 MST", publishDate + " WIB")
-
-	if err != nil {
-		panic(err)
-	}
-
-	return t
-}
-
-func (c *CnnNewsContent) GetCreatedAt() time.Time {
-	publishDate := GetMeta(c.html, "dtk:createddate")
-	t, err := time.Parse("2006/01/02 15:04:05 MST", publishDate + " WIB")
-
-	if err != nil {
-		panic(err)
-	}
-
-	return t
-}
 
 func NewLinkExtractor(u string) LinkExtractor {
 	parsedUrl, err := url.Parse(u)
